63b9822be30e287763d9c620cb5017a5b2a241e0
jerryHo914 <jerryjp3914@gmail.com>
2019-12-21 00:52:42 +0800

    bubble download modulize

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import imgDownload
+import bubbleDownload
 
 rootUrl = 'https://sites.google.com/junyiacademy.org/linkit7697/'
 route = 'getstart/基礎電學觀念'
@@ -17,13 +18,18 @@ sectionText = []
 sectionImgCount = []
 imgPath = []
 imgIdx = 1
+bubbleIdx = 1
 for section in sectionBlock:
     if(section.text==''):
         continue
     img = section.find_all('img')
+    if(section.select(".w536ob")):
+        bubbleDownload.download_single_bubble(section.select(".w536ob"),bubblefilePath+'/'+str(bubbleIdx))
+        bubbleIdx+=1
+    
     sectionImgCount.append(len(img))
     for i in range(len(img)):
-        imgDownload.download_single_img(img[i]["src"],imgfilePath+'/'+str(imgIdx))
+        # imgDownload.download_single_img(img[i]["src"],imgfilePath+'/'+str(imgIdx))
         imgIdx+=1
     
     sectionText.append(section.text)
--- src/bubbleDownload.py
+++ src/bubbleDownload.py
@@ -3,34 +3,20 @@ from bs4 import BeautifulSoup
 import os
 
 
-rootUrl = 'https://sites.google.com/junyiacademy.org/linkit7697/'
-route = 'getstart/基礎電學觀念/'
-url = rootUrl+route
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'html.parser')
-fileRootPath = str('D:/workspace/HtmlParseBot/Linkit7697Learning Resources/bubble/')
-filePath = fileRootPath+route
-
-
-sectionBlock = soup.select(".w536ob")
-idx=1
-for d in sectionBlock:
-    # print(d)
+def download_single_bubble(d,filePath):
     try: 
         domIdx = str(d).find("&lt;!DOCTYPE")
         domEnd = str(d).find("&lt;/html&gt;")
-        
         content = str(d)[domIdx:domEnd]+"&lt;/html&gt;"
         content = content.replace("&lt;","<")
         content = content.replace("&gt;",">")
         print(content)
-        html_out = open(filePath+'/'+str(idx)+".html",'w',encoding = 'utf8')
+        html_out = open(filePath+".html",'w',encoding = 'utf8')
         html_out.write(content)
         html_out.close()
-        idx+=1
     except Exception as e:
         print(e)
 
 # https://jerry914.github.io/Linkit7697Learning-Resources/bubble/DIR.html
 
-print("爬蟲結束")
+print("bubble Dowmload !")
