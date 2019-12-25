b964c4ed4a1ee13729aedb130d584ecc8ffcd212
jerry <joyce91400@hotmail.com>
2019-12-25 17:02:26 +0800

    wrong arch

--- src/generator.py
+++ src/generator.py
@@ -5,7 +5,7 @@ import sqlite3
 
 def titleSplit(section,subtitle):
     title = section.split("|")
-    mdtitle = title[1]+"\n"+title[2].replace(subtitle,'')
+    mdtitle = title[1]+"\\n"+title[2].replace(subtitle,'')
     return mdtitle
 
 def artBlk_generate(num,Type):
@@ -18,82 +18,41 @@ def artBlk_generate(num,Type):
     else:
         blockType = "iframe"
     for n in range(num):
-        string+="[[☃ "+blockType+" "+str(n+1)+"]]\n\n"
+        string+="[[☃ "+blockType+" "+str(n+1)+"]]\\n\\n"
     return string
 
 def artBlk_wigit_generate(num,Type,route,idx):
-    string = ''
+    resuleString = ''
+    artTemplete = junyiJSONgenerate.JSON_open('src\Templete\JSONartblo')
     if(Type == 'm'):
         for n in range(num):
-            string+='''"image '''+str(n+1)+'''": {
-                            "graded": true,
-                            "id": "image '''+str(n+1)+'''",
-                            "options": {
-                                "aspectRatio": 0.3352941176470588,
-                                "backgroundImage": {
-                                    "url": "https://jerry914.github.io/Linkit7697Learning-Resources/img/'''+route+'''+/'''+str(idx)+'''.gif",
-                                    "width": 435
-                                },
-                                "box": [],
-                                "labels": [],
-                                "maxImageSize": 435,
-                                "range": [
-                                    [
-                                        0,
-                                        10
-                                    ],
-                                    [
-                                        0,
-                                        10
-                                    ]
-                                ],
-                                "useBoxSize": false
-                            },
-                            "type": "image",
-                            "version": {
-                                "major": 0,
-                                "minor": 0
-                            }
-                        },'''
-        return string[:-1]
+            templete = junyiJSONgenerate.JSON_open('src\Templete\JSONimage')
+            string = templete.replace("IMGBLOCKINDEX",str(n+1))
+            string = string.replace("ROUTE",route)
+            string = string.replace("IMGIDX",str(idx))
+            resuleString+=string
+        return resuleString[:-1]
     else:
         for n in range(num):
-            string+='''"iframe '''+str(n+1)+'''": {
-                "type": "iframe",
-                "graded": true,
-                "options": {
-                    "url": "https://jerry914.github.io/Linkit7697Learning-Resources/bubble/'''+route+'/'+str(idx)+'''.html",
-                    "settings": [
-                        {
-                            "name": "",
-                            "value": ""
-                        }
-                    ],
-                    "width": 560,
-                    "height": 320
-                },
-                "version": {
-                    "major": 0,
-                    "minor": 0
-                }
-            },'''
-        return string
+            templete = junyiJSONgenerate.JSON_open('src\Templete\JSONiframe')
+            string = templete.replace("IFRAMEIDX",str(n+1))
+            string = string.replace("ROUTE",route)
+            string = string.replace("BUBBLEIDX",str(idx))
+            resuleString+=string
+        return resuleString
 
 def cont_wiget_generate(sectionText,sectionImgCount,sectionBubbleCount,route):
-    templete = junyiJSONgenerate.JSON_openTemplete()
-    footerContent = templete["question"]["widgets"]["article-block 1"]["options"]["widgets"]
-    # bubbleIdx = 1
-    # imgIdx = 1
+    templete = junyiJSONgenerate.JSON_open('src\Templete\JSONartblo')
+    bubbleIdx = 1
+    imgIdx = 1
+    result =  ''
+    for a in range(len(sectionText)-2):
+        articleBlock = templete.replace("ARTICLEIDX",str(a))
+        articleBlock = articleBlock.replace("IFRAMEBLOCK",artBlk_wigit_generate(a,'f',route,bubbleIdx))
+        articleBlock = articleBlock.replace("IMGBLOCK",artBlk_wigit_generate(a,'m',route,imgIdx))
+        bubbleIdx+=len(sectionBubbleCount)
+        imgIdx+=len(sectionImgCount)
+        result+=articleBlock
 
-    # temp = json.dumps({"article-block 1": footerContent})
-    # temp=json.dumps({"article-block 1": {"age":"fff"}})
-    print(footerContent)
-    # for a in range(len(sectionText)-2):
-    #     articleBlock = junyiJSONgenerate.JSON_open('src\Templete\JSONartblo')
-    #     json = json.dumps({'article-block'+a: articleBlock['article-block 1']})
-    #     bubbleIdx+=len(sectionBubbleCount)
-    #     imgIdx+=len(sectionImgCount)
-
-    widget = footerContent
-    return widget
+    return result
                         
--- src/junyiJSONgenerate.py
+++ src/junyiJSONgenerate.py
@@ -3,26 +3,27 @@ from pprint import pprint
 import generator
 
 def JSON_openTemplete():
-    with open("src/Templete/JSONtemplete.json", 'r',encoding = 'utf8') as tf: 
-        templete = json.load(tf)
+    with open("src\Templete\JSONtemplete.txt", 'r',encoding = 'utf8') as tf: 
+        templete = tf.read()
         tf.close()
         return templete
 
 def JSON_open(fileName):
-    with open(fileName+".json", 'r',encoding = 'utf8') as f: 
-        data = json.load(f)
-        pprint(data)
+    with open(fileName+".txt", 'r',encoding = 'utf8') as f: 
+        data = f.read()
         f.close()
         return data
 
 def JSON_generate(fileName,sectionText,subtitle,sectionImgCount,sectionBubbleCount,route):
     templete = JSON_openTemplete()
-    templete["question"]["content"] = "# "+generator.titleSplit(sectionText[0],subtitle)+"\n\n[[☃ article-block 1]]"
-    templete["question"]["widgets"]["article-block 1"]["options"]["articleBlock"] = "# "+subtitle+"\n"+sectionText[1]+"\n"+generator.artBlk_generate(len(sectionText)-2,'a')+"[[☃ explanation 1]]\n\n[[☃ explanation 2]]\n\n[[☃ image 1]]\n\n[[☃ image 2]]"
-    # templete["question"]["widgets"]["article-block 1"]["options"]["widgets"] = generator.cont_wiget_generate(sectionText,sectionImgCount,sectionBubbleCount,route)
+    content = templete.replace('CONTENTTITLE',generator.titleSplit(sectionText[0],subtitle))
+    content = content.replace("SUBTITLE\nSECTION[1]",subtitle+"\\n"+sectionText[1])
+    content = content.replace("[[☃ article-block 1~SECTIONLEN-2]]",generator.artBlk_generate(len(sectionText)-2,'a'))
+    content = content.replace("ARTICLEBLOCK",generator.cont_wiget_generate(sectionText,sectionImgCount,sectionBubbleCount,route))
     
-    with open(fileName+".txt", 'w',encoding = 'utf8') as f:
-        f.write(str(templete).replace("'",'"'))
+    with open(fileName+".txt", 'w',encoding = 'utf8') as f: 
+        f.write(content)
+        # print(templete)
         f.close()
 
 
